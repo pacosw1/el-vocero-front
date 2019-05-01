71ab5195df6f94b6fe8bd857431e89cd07e8b826
Paco <pacoswilliams@gmail.com>
2019-04-30 22:47:53 -0500

    build

---
+++ build/precache-manifest.bc49b4a765ee5e633aae3634e0394d3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4564db6d5cc06ce87d3e64f01127ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d78cd4ac584577010b35",
+    "url": "/static/css/2.c489bcc4.chunk.css"
+  },
+  {
+    "revision": "3657b6f23beaf954af69",
+    "url": "/static/css/main.6d0eb18b.chunk.css"
+  },
+  {
+    "revision": "d78cd4ac584577010b35",
+    "url": "/static/js/2.8d7cc008.chunk.js"
+  },
+  {
+    "revision": "3657b6f23beaf954af69",
+    "url": "/static/js/main.b18f7906.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
